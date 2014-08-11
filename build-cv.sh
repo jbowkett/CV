148ae5583ca1247a34b846f240cf8fc8b86b8d71
jbowkett <james_bowkett@lyagushka.co.uk>
2014-08-11 20:32:07 +0000

    Added footer to add page numbers

--- build-cv.sh
+++ build-cv.sh
@@ -1,1 +1,1 @@
-gimli -file README.md -outputfilename James-Bowkett-CV -stylesheet style.css
+gimli -file README.md -outputfilename James-Bowkett-CV -stylesheet style.css -w "--footer-right [page]  --footer-font-size 8"
